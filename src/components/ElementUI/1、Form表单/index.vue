9fc7e6765bffb5deefaae6de6cab1e91395ba73d
王成阳 <985341454@qq.com>
2021-06-18 17:08:11 +0800

    提交form表单样例

--- src/components/ElementUI/1、Form表单/index.vue
+++ src/components/ElementUI/1、Form表单/index.vue
@@ -1,57 +1,90 @@
 <template>
     <div class="content">
         <h1>Form表单</h1>
-        <div class="maincontent" id="PickUpOrderEdit">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="基本信息" name="1"></el-tab-pane>
+            <el-tab-pane label="其它信息" name="2"></el-tab-pane>
+        </el-tabs>
+        <div class="maincontent">
             <el-form :model="updateForm" ref="updateForm" size="small" label-width="100px" :validate-on-rule-change="true">
-                <el-row :gutter="20">
-                    <el-col :span="6">
-                        <el-form-item label="产品名称:" prop="ProductName">
-                        <el-input v-model="updateForm.ProductName" :disabled="true"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-form-item label="用车类型:" prop="AirportTransferType">
-                        <el-input v-model="updateForm.AirportTransferType" :disabled="true"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-                <el-table
-                :data="updateForm.CustomerInfosList"
-                stripe
-                border
-                header-cell-class-name="tableheader"
-                style="width: 100%">
-                    <el-table-column
-                        prop="CustomerName"
-                        label="姓名"
-                        align="center">
-                        <template slot-scope="scope">
-                        <el-form-item :prop="'CustomerInfosList.' + scope.$index + '.CustomerName'" :rules="rules.CustomerName">
-                            <el-input size="small" v-model="scope.row.CustomerName"></el-input>
-                        </el-form-item>
-                        </template>
-                    </el-table-column>
-                    <el-table-column
-                        prop="CustomerIdType"
-                        label="证件类型"
-                        align="center">
-                        <template slot-scope="scope">
-                        <el-form-item :prop="'CustomerInfosList.' + scope.$index + '.CustomerIdType'" :rules="rules.CustomerIdType">
-                            <el-select v-model="scope.row.CustomerIdType" placeholder="请选择" @change="customerIdTypeChange($event, scope.$index)">
-                                <el-option v-for="item in idType"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value"
-                                size="small">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                        </template>
-                    </el-table-column>
-                </el-table>
-                <el-form-item class="submit-radio">
-                <el-button type="primary" size="mini" @click="submit('updateForm')">确定</el-button>
-                </el-form-item>
+                <div v-if="activeName == 1">
+                    <el-row :gutter="20">
+                        <el-col :span="6">
+                            <el-form-item label="姓名:" prop="name" :rules="rules.name">
+                                <el-input v-model="updateForm.name"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="6">
+                            <el-form-item label="性别:" prop="sex" :rules="rules.sex">
+                                <el-select v-model="updateForm.sex" placeholder="请选择">
+                                    <el-option v-for="item in sexList"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value"
+                                    size="small">
+                                    </el-option>
+                                </el-select>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                    <el-table
+                        :data="updateForm.goodsList"
+                        stripe
+                        border
+                        header-cell-class-name="tableheader"
+                        style="width: 100%">
+                        <el-table-column
+                            prop="CustomerName"
+                            label="商品名称"
+                            align="center">
+                            <template slot-scope="scope">
+                                <el-form-item :prop="'goodsList.' + scope.$index + '.goodName'" :rules="rules.goodName">
+                                    <el-input size="small" v-model="scope.row.goodName"></el-input>
+                                </el-form-item>
+                            </template>
+                        </el-table-column>
+                        <el-table-column
+                            prop="CustomerIdType"
+                            label="商品类型"
+                            align="center">
+                            <template slot-scope="scope"> 
+                                <el-form-item :prop="'goodsList.' + scope.$index + '.goodType'" :rules="rules.goodType">
+                                    <el-select v-model="scope.row.goodType" placeholder="请选择" style="width:100%;margin-left:0px">
+                                        <el-option v-for="item in sexList"
+                                        :key="item.value"
+                                        :label="item.label"
+                                        :value="item.value"
+                                        size="small">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </template>
+                        </el-table-column>
+                        <el-table-column align="center">
+                            <template slot="header" slot-scope="scope">
+                                <i class="el-icon-circle-plus" @click="handleAdd()"></i>
+                            </template>
+                            <template slot-scope="scope" v-if="scope.$index != 0">
+                                <i class="el-icon-remove" @click="handleDelete(scope.$index)"></i>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
+                <div v-if="activeName == 2">
+                    <el-row :gutter="20">
+                        <el-col :span="6">
+                            <el-form-item label="工作:" prop="work" :rules="rules.work">
+                                <el-input v-model="updateForm.work"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="6">
+                            <el-form-item label="地址:" prop="address" :rules="rules.address">
+                                <el-input v-model="updateForm.address"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                </div>
+                <el-button type="primary" size="mini" @click="submit('updateForm')" style="margin-top:20px">确定</el-button>
             </el-form>
         </div>
     </div>
@@ -61,34 +94,66 @@
 export default {
   data () {
         return {
+            activeName: '1',
             updateForm: {
-                CustomerInfosList: [{CustomerName: '', CustomerIdType: 1, CustomerIdNum: '', CustomerPhone: '', DepAddress: '', ArrAddress: '', Sequence: 0}]
+                goodsList: [{goodName: '', goodType: ''}]
             },
             rules: {
-                CustomerName: [
-                    { required: true, message: '乘客姓名不能为空', trigger: ['blur'] },
-                    { max: 20, message: '乘客姓名不能超过20个字符，请重新输入', trigger: 'blur' }
+                name: [
+                    { required: true, message: '姓名不能为空', trigger: ['blur'] },
                 ],
-                CustomerIdType: [
-                    {required: true, message: '请选择证件类型', trigger: 'change'}
-                ]
+                sex: [
+                    {required: true, message: '性别不能为空', trigger: 'change'}
+                ],
+                goodName: [
+                    { required: true, message: '商品名称不能为空', trigger: ['blur'] },
+                ],
+                goodType: [
+                    {required: true, message: '商品类型不能为空', trigger: 'change'}
+                ],
+                work: [
+                    {required: true, message: '工作不能为空', trigger: ['blur']}
+                ],
+                address: [
+                    {required: true, message: '地址不能为空', trigger: ['blur']}
+                ],
+
             },
-            idType: [{value: 1, label: '身份证'}, {value: 2, label: '护照'}, {value: 3, label: '港澳台居民居住证'}, {value: 4, label: '其他'}],
+            sexList: [{value: 1, label: '男'}, {value: 2, label: '女'}],
         }
     },
     created(){
     },
  
     methods: {
+        handleClick() {
+            this.$nextTick(() => {
+                this.$refs.updateForm.clearValidate()
+            })
+        },
         submit (form) {
             this.$refs[form].validate((valid) => {
                 if (valid) {
                     this.$message({message:'保存成功',type:'success'})
-                } else {
-                    this.$message({message:'校验不通过',type:'warning'})
+                    console.log(this.updateForm);
                 }
             })
-        }
+        },
+        handleAdd () {
+            if (this.updateForm.goodsList.length < 3) {
+                let customerInfo = {}
+                this.updateForm.goodsList.push(customerInfo)
+            } else {
+                this.$message({
+                message: '最多添加三条',
+                type: 'warning',
+                duration: 3000
+                })
+            }
+        },
+        handleDelete (index) {
+            this.updateForm.goodsList.splice(index, 1)
+        },
     },
 }
 </script>
